8801cac325a4de0302ce4c7bbcb3c3721c46c5d8
Anupam Walker <singhsamarth656@gmail.com>
2021-09-03 06:47:32 +0530

    Update msg.py

--- VCPlayBot/modules/msg.py
+++ VCPlayBot/modules/msg.py
@@ -5,7 +5,7 @@ from VCPlayBot.config import PROJECT_NAME
 from VCPlayBot.config import SUPPORT_GROUP
 from VCPlayBot.config import UPDATES_CHANNEL
 class Messages():
-      START_MSG = "**Hello [{}](tg://user?id={})!**\n\ I am an advanced bot created for playing music in the voice chats of Telegram Groups & Channels.\n\n Send me /help for more info.\n\n Join @Girls_And_Boys_Chatting"
+      START_MSG = "**Hello [{}](tg://user?id={})!**\n I am an advanced bot created for playing music in the voice chats of Telegram Groups & Channels.\n\n Send me /help for more info.\n\n Kindly message @aw63933 if you're facing errors."
       HELP_MSG = [
         ".",
 f"""
